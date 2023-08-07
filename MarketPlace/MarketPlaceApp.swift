cce77ef53011d3dc9975a8f4b096e1834204a8d6
austinwilson25 <austinwilson@mail.weber.edu>
2023-08-07 08:56:26 -0600

    Initial Commit

---
+++ MarketPlace/MarketPlaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarketPlaceApp.swift
+//  MarketPlace
+//
+//  Created by Sadie Wilson on 8/7/23.
+//
+
+import SwiftUI
+
+@main
+struct MarketPlaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
